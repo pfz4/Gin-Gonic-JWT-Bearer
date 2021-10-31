f05f7973a20a959cab2fcf5697863624e1efdd69
Paul Z <info@pfz4.de>
2021-10-31 20:54:50 +0100

    Added RequireScope, RequireRole Middleware

--- ginmiddleware.go
+++ ginmiddleware.go
@@ -3,6 +3,7 @@ package jwtbearer
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
 func (conf *JwtBearer) LoadJWTBearer() gin.HandlerFunc {
@@ -28,6 +29,58 @@ func (conf *JwtBearer) RequireJWTBearer() gin.HandlerFunc {
 		tokenInfo = conf.ValidateOrigin(tokenInfo, c.Request.Header.Get("Origin"))
 		if !tokenInfo.IsValid {
 			c.JSON(http.StatusForbidden, gin.H{"msg": tokenInfo.ErrorMessage})
+			c.Abort()
+			return 
+		}
+
+		c.Set("tokeninfo", tokenInfo)
+		c.Next()
+	}
+}
+
+func (conf *JwtBearer) RequireScope(scope string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		tokenInfo, err := conf.GetTokenInfoFromAuthHeader(c.Request.Header.Get("Authorization"))
+		if err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "The Token is invalid"})
+			c.Abort()
+			return
+		}
+		tokenInfo = conf.ValidateOrigin(tokenInfo, c.Request.Header.Get("Origin"))
+		if !tokenInfo.IsValid {
+			c.JSON(http.StatusForbidden, gin.H{"msg": tokenInfo.ErrorMessage})
+			c.Abort()
+			return 
+		}
+		if !sliceContainsValue(strings.Split(tokenInfo.Scope, " "), scope) {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "Required Scope not found"})
+			c.Abort()
+			return 
+		}
+
+		c.Set("tokeninfo", tokenInfo)
+		c.Next()
+	}
+}
+
+func (conf *JwtBearer) RequireRole(role string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		tokenInfo, err := conf.GetTokenInfoFromAuthHeader(c.Request.Header.Get("Authorization"))
+		if err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "The Token is invalid"})
+			c.Abort()
+			return
+		}
+		tokenInfo = conf.ValidateOrigin(tokenInfo, c.Request.Header.Get("Origin"))
+		if !tokenInfo.IsValid {
+			c.JSON(http.StatusForbidden, gin.H{"msg": tokenInfo.ErrorMessage})
+			c.Abort()
+			return 
+		}
+		if !sliceContainsValue(tokenInfo.Roles, role) {
+			c.JSON(http.StatusForbidden, gin.H{"msg": "Required Role not found"})
 			c.Abort()
 			return 
 		}
